07beb478db997192492c55c72c3630effb977c0a
Theophile Villard <theophile.villard@gmail.com>
2018-03-22 14:12:49 +0100

    add live truffle conf

--- truffle.js
+++ truffle.js
@@ -13,6 +13,13 @@ module.exports = {
       network_id: 4,
       gas: 4700000,
       gasPrice: 21000000000
+    },
+    live: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: 1,
+      gas: 4700000,
+      gasPrice: 3000000000
     }
   }
 };
